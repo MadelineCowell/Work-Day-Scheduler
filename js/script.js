16c9c05da89bb445cfbfe3e50b75ba376ce7a097
MadelineC <madeline.cowell@colorado.edu>
2020-10-04 18:28:17 -0600

    updating js

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// create an array for timeblocks
+
+// loop to add timeblocks to container
+
+$(document).ready(function() {
+    var hours = {
+        amHours: [7, 8, 9, 10, 11],
+        pmHours: [12, 1, 2, 3, 4, 5, 6]
+    };
+
+    for (var i=0; i< hours.amHours.length;  i++) {
+        var newRow = $('<div class='row'></div>');
+        $('.container').append(newRow);
+        var hrCol = $(`<div class='col-1 time-block'>${hours.amHours[i]}AM</div>`);
+        var textCol = $(`<textarea class="col-10" data-time=${colorChangeObject.amColorHours[i]}>Enter your notes here...</textarea>`);
+        var buttonCol = $('<button class="col-1 saveBtn">Save</button>');
+        $(newRow).append(hrCol, textCol, buttonCol);
+    }
+})
