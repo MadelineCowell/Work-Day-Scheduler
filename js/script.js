a97ffcfe738dbf8c9b6e9d46fe61c02c9d3d7e7c
MadelineC <madeline.cowell@colorado.edu>
2020-10-26 15:39:22 -0600

    updated timeblocks

--- js/script.js
+++ js/script.js
@@ -1,95 +1,48 @@
-// create an array for timeblocks
+    // time variables 
+    var hour = [7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
+    var displayHour = ["9:00am", "10:00am", "11:00am", "12:00pm", "1:00pm", "2:00pm", "3:00pm", "4:00pm", "5:00pm"];
 
-// loop to add timeblocks to container
+    // Displaying current time
+    $("#currentDay").text(moment().format("dddd, MMMM Do"));
+    // var currentTime = moment();
 
-$(document).ready(function() {
-    $(".saveBtn").on("click", function() {
-        var value = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
 
-        localStorage.setItem(time, value);
-    });
+    // looping hours and current time
 
-    function updateHour() {
-        var currentHour = moment().hours();
-        $(".time-block").each(function() {
-            var hour = parseInt($(this).attr("id").split(""));
-            if (hour < currentHour) {
-                $(this).addClass("past");
-            } else if (hour === currentHour) {
-                $(this).removeClass("past");
-                $(this).addClass("present");
-            } else {
-                $(this).removeClass("past");
-                $(this).removeClass("present");
-                $(this).addClass("future");
-            }
-        });
+    for (i = 0; i < hour.length; i++) {
+        var createDiv = $(` <div id = "hour-${hour[i]}" class = "row time-block" value = ${hour[i]} > `);
+        var createTextArea = $("<textarea>");
+        $(".container").append(createDiv);
+        var currentTime = displayHour[i];
 
+        createDiv.append($("<div class = 'col-10 hour' >").text(currentTime));
+        createDiv.append(createTextArea.attr("class", "col-10 description").attr("id", "memo-area"));
+        createDiv.append($("<button class = 'col-1 saveBtn button' >").text("save"));
+
+        if (moment().format("Hour") == hour[i]) {
+            $("#hour-" + hour[i]).addClass("present")
+        } else if (moment().format("Hour") > hour[i]) {
+            $("#hour-" + hour[i]).addClass("past")
+        } else if (moment().format("Hour") < hour[i]) {
+            $("#hour-" + hour[i]).addClass("future")
+        }
     }
-    
-    updateHour();
 
-    var interval = setInterval(updateHour, 15000);
+    $(".saveBtn").on("click", function () {
+        var value = $(this).siblings(".description").val();
+        var key = $(this).parent().attr("id");
+        localStorage.setItem(key, value);
+    })
 
-    // load data from local storage
-
+    //loading in local storage
     $("#hour-7 .description").val(localStorage.getItem("hour-7"));
     $("#hour-8 .description").val(localStorage.getItem("hour-8"));
     $("#hour-9 .description").val(localStorage.getItem("hour-9"));
-    $("#hour-9 .description").val(localStorage.getItem("hour-10"));
-    $("#hour-9 .description").val(localStorage.getItem("hour-11"));
-    $("#hour-9 .description").val(localStorage.getItem("hour-12")); 
-    $("#hour-9 .description").val(localStorage.getItem("hour-1"));
-    $("#hour-9 .description").val(localStorage.getItem("hour-2"));
-    $("#hour-9 .description").val(localStorage.getItem("hour-3"));
-    $("#hour-9 .description").val(localStorage.getItem("hour-4"));
-    $("#hour-9 .description").val(localStorage.getItem("hour-5"));
-
-
-   
-    // // AM hours
-
-    // for (var i=0; i< hours.amHours.length;  i++) {
-    //     var newRow = $('<div class='row'></div>');
-    //     $('.container').append(newRow);
-    //     var hrCol = $(`<div class='col-1 time-block'>${hours.amHours[i]}AM</div>`);
-    //     var textCol = $(`<textarea class="col-10" data-time=${colorChangeObject.amColorHours[i]}>Enter your notes here...</textarea>`);
-    //     var buttonCol = $('<button class="col-1 saveBtn">Save</button>');
-    //     $(newRow).append(hrCol, textCol, buttonCol);
-    // }
-
-    // // PM hours
-
-    // for (var i=0; i< hours.pmHours.length;  i++) {
-    //     var newRow = $('<div class='row'></div>');
-    //     $('.container').append(newRow);
-    //     var hrCol = $(`<div class='col-1 time-block'>${hours.pmHours[i]}AM</div>`);
-    //     var textCol = $(`<textarea class="col-10" data-time=${colorChangeObject.pmColorHours[i]}>Enter your notes here...</textarea>`);
-    //     var buttonCol = $('<button class="col-1 saveBtn">Save</button>');
-    //     $(newRow).append(hrCol, textCol, buttonCol);
-    // } 
-
-    // // loading local storage    
-    // var load = localStorage.getItem("storage"); 
-    // if(load !== null) {
-    //     storage = JSON.parse(load);
-    //     $("textarea").each(function(index) {
-    //         $(this).val(storage [7 + index]);
-    //     });
-    // }
-
-    //Displaying current time
-    $("#currentDay").text(dayjs().format("ddd, MMM DD, YYYY"));
-
-
-//     // Saving data to local storage
-
-//     $("button").on("click", function () { //when button is clicked, get content, and save it to temp storage, then send to local storage
-//         var str = $(this).prev().val();
-//         var idx = $(this).prev().prev().attr("data-time");
-//         storage[idx] = str;
-//         localStorage.setItem("storage", JSON.stringify(storage));
-//       });
-
-// })
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"))
